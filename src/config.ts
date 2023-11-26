303b371eaef0be2d5ceffaf4c5705dad2e1f0efc
MazenMarei <moazmazen9199@gmail.com>
2023-11-26 19:43:22 +0200

    1

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export default {
-    token       :"MTE3NTg2MTgxNTA5MjE5OTQ1NA.GV_gCV.YpFJPGSIKlmgWjte6uquKCUvgCMNhloOoQ9lB0",
+    token       :"",
     prefix      : "-",
-    Mongoose    :"mongodb+srv://kils:011maz787@cluster0.d3re0.mongodb.net/Buttons_bot",
+    Mongoose    :"",
     debugMode   : true,
 }
